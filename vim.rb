d0cba13f02b4c9dd301c26630e1b9875dae0e84c
Charles G <charles@chuckg.org>
2012-03-21 10:21:04 -0700

    Added vim formula

---
+++ vim.rb
@@ -0,0 +1,27 @@
+require 'formula'
+
+class Vim < Formula
+  # Get stable versions from hg repo instead of downloading an increasing
+  # number of separate patches.
+  url 'https://vim.googlecode.com/hg/', :revision => '70eff6af1158'
+  version '7.3.462'
+  homepage 'http://www.vim.org/'
+
+  head 'https://vim.googlecode.com/hg/'
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--mandir=#{man}",
+                          "--enable-gui=no",
+                          "--without-x",
+                          "--disable-nls",
+                          "--enable-multibyte",
+                          "--with-tlib=ncurses",
+                          "--enable-pythoninterp",
+                          "--enable-rubyinterp",
+                          "--with-ruby-command=/usr/bin/ruby",
+                          "--with-features=huge"
+    system "make"
+    system "make install"
+  end
+end
